1b580db16c2a4ec6e62760de35b10fd7d6675c70
Dfectv <psilopsyben@gmail.com>
2022-09-30 14:54:59 -0700

    Pushing main and makefile for final version

---
+++ main.cpp
@@ -0,0 +1,141 @@
+#include "ComplexPlane.h"
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	//Get video resolution
+	VideoMode vm(1920, 1080);
+	VideoMode desktop = vm.getDesktopMode();
+	float desktopWidth = desktop.width;
+	float desktopHeight = desktop.height;
+	
+	//Calculate aspect ratio
+	float aspectRatio = desktopHeight / desktopWidth;
+	
+	//Construct window
+	RenderWindow window(vm, "Mandlebrot Set", Style::Default);
+
+	//Construct ComplexPlane object
+	ComplexPlane complexPlane(aspectRatio);
+
+	//Construct Font object
+	Font font;
+	font.loadFromFile("OpenSans-Regular.ttf");
+
+	//Construct Text object and set font to text
+	Text text;
+	text.setFont(font);
+
+	//Construct a VertexArray
+	VertexArray background;
+	//Set primitive type to Points
+	background.setPrimitiveType(Points);
+	//Resize the screen to height * width
+	background.resize(desktopHeight * desktopWidth);
+
+	//Create an enum class state variable
+	enum State { CALCULATING, DISPLAYING };
+	//Initialize to CALCULATING
+	State state = CALCULATING;
+
+	//Begin loop
+	while (window.isOpen())
+	{
+		//Handle player's input
+
+		//Create queue events
+		Event event;
+		while (window.pollEvent(event))
+		{
+			//Event for closing window
+			if(event.type == Event::Closed)	{ window.close(); }
+			
+			//Event if mouse button is pressed
+			if (event.type == Event::MouseButtonPressed)
+			{
+				//Find the coordinates of the mouse click
+				Vector2i points = Mouse::getPosition(window);
+				Vector2f coord = window.mapPixelToCoords(points, complexPlane.getView());
+
+				//ZoomOut and SetCenter if right mouse button is clicked
+				if (event.mouseButton.button == Mouse::Right)
+				{
+					complexPlane.zoomOut();
+					complexPlane.setCenter(coord);
+				}
+
+				//ZoomIn and SetCenter if left mouse button is clicked
+				else if (event.mouseButton.button == Mouse::Left)
+				{
+					complexPlane.zoomIn();
+					complexPlane.setCenter(coord);
+				}
+
+				//Set state to CALCULATING
+				state = CALCULATING;
+			}
+
+			//Event when mouse moves
+			if (event.type == Event::MouseMoved)
+			{
+				//Get coordinates of mouse location
+				Vector2i points = Mouse::getPosition(window);
+				Vector2f coord = window.mapPixelToCoords(points, complexPlane.getView());
+
+				//Store mouse location
+				complexPlane.setMouseLocation(coord);
+			}
+
+			//Event when escape key is pressed
+			if(event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
+			{ window.close(); }
+		}
+
+		//Update scene
+		if (state == CALCULATING)
+		{
+			//Make double for loop for all pixels
+			for(int j = 0; j < static_cast<int>(desktopWidth); j++)
+			{
+				for(int i = 0; i < static_cast<int>(desktopHeight); i++)
+				{
+					background[j + i * static_cast<int>(desktopWidth)].position = {(float)j, (float)i};
+					Vector2i points(j, i);
+					Vector2f coord = window.mapPixelToCoords(points, complexPlane.getView());
+
+					//Call countIterations and store return value
+					size_t count = complexPlane.countIterations(coord);
+
+					//Declare 3 local variables r, g, b
+					Uint8 r, g, b;
+
+					//Pass count and 3 color variables to iterationsToRGB
+					complexPlane.iterationsToRGB(count, r, g, b);
+
+					//Set color variable in VertexArray
+					background[j + i * static_cast<int>(desktopWidth)].color = {r, g, b};
+				}
+			}
+			
+			//Set state to DISPLAYING
+			state = DISPLAYING;
+		}
+
+		//Draw scene
+
+		//Call loadText
+		complexPlane.loadText(text);
+
+		//Clear window
+		window.clear();
+
+		//Draw VertexArray
+		window.draw(background);
+
+		//Draw text
+		window.draw(text);
+
+		//Display window
+		window.display();
+	}
+}
